1dfe4f5f2494795c685aedaae0c0780afabd6eb4
ayuram <54257595+ayuram@users.noreply.github.com>
2021-07-08 17:25:50 -0700

    g sign in

--- lib/backend.dart
+++ lib/backend.dart
@@ -192,23 +192,24 @@ class AuthenticationService {
     await _firebaseAuth.signOut();
   }
 
-  Future<UserCredential> signInWithGoogle() async {
+  Future<UserCredential?> signInWithGoogle() async {
     // Trigger the authentication flow
-    final GoogleSignInAccount googleUser =
-        await (GoogleSignIn().signIn() as FutureOr<GoogleSignInAccount>);
+    final GoogleSignInAccount? googleUser = await (GoogleSignIn().signIn());
+    if (googleUser != null) {
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication googleAuth =
-        await googleUser.authentication;
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
 
-    // Create a new credential
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    // Once signed in, return the UserCredential
-    return await _firebaseAuth.signInWithCredential(credential);
+      // Once signed in, return the UserCredential
+      return await _firebaseAuth.signInWithCredential(credential);
+    }
+    return null;
   }
 
   String generateNonce([int length = 32]) {
