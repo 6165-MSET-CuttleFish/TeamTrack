15bf284305acc4feb05e9fc93760499a82c716fb
ayuram <54257595+ayuram@users.noreply.github.com>
2021-10-24 20:02:33 -0700

    documentation

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -4,9 +4,11 @@ import {ftcAPIKey} from "./api/ftcAPIKey";
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 admin.initializeApp();
+
+// put event in desired user's inbox
 export const shareEvent = functions.https.onCall(async (data, context) => {
   functions.logger.info("Event share", {structuredData: true});
-  if (!context.auth) {
+  if (!context.auth) { // if not authenticated
     throw new functions.https.HttpsError(
         "unauthenticated",
         "User not logged in"
@@ -18,25 +20,26 @@ export const shareEvent = functions.https.onCall(async (data, context) => {
   const recipient = await admin
       .auth()
       .getUserByEmail(data.email);
-  if (recipient == null) {
+  if (recipient == null) { // if recipient doesn't exist
     throw new functions.https.HttpsError(
         "invalid-argument",
         "Requested user does not exist"
     );
   }
-  if (sender.uid == recipient.uid) {
+  if (sender.uid == recipient.uid) { // if sender and recipient are the same
     throw new functions.https.HttpsError(
         "invalid-argument",
         "Cannot send an event to yourself"
     );
   }
-  await admin.database().ref().child("Events")
-      .child(data.gameName).child(data.id)
-      .child("Permissions").child(recipient.uid).set({
+  await admin.database().ref()
+      .child(`Events/${data.gameName}/${data.id}/Permissions/${recipient.uid}`)
+      .set({
         "role": data.role,
         "name": recipient.displayName,
         "email": recipient.email,
-      });
+        "photoURL": recipient.photoURL,
+      }); // update permissions for recepient
   const meta = {
     "id": data.id,
     "name": data.name,
@@ -55,11 +58,12 @@ export const shareEvent = functions.https.onCall(async (data, context) => {
   const returnVal = await admin.firestore().runTransaction(async (t) => {
     const doc = await t.get(ref);
     const newInbox = doc?.data()?.inbox;
+    // save the fcm tokens to send to
     tokens = doc?.data()?.FCMtokens;
     const blocked = doc?.data()?.blockedUsers;
-    if (blocked[sender.uid] != null) allowSend = false;
-    if (newInbox[data.id] != null ||
-      doc?.data()?.events[data.id] != null) allowSend = false;
+    // allow send if not blocked and not in inbox and not already shared
+    allowSend = blocked[sender.uid] == null && newInbox[data.id] == null &&
+      doc?.data()?.events[data.id] == null;
     if (allowSend) {
       newInbox[data.id] = meta;
     }
@@ -74,17 +78,25 @@ export const shareEvent = functions.https.onCall(async (data, context) => {
     notification: notification,
   };
   if (tokens.length != 0 && allowSend) {
-    await admin.messaging().sendMulticast(message);
+    await admin.messaging().sendMulticast(message); // send notifications
   }
   return returnVal;
 });
 
+// update creator's permissions and add the new event to creator's events list
 export const nativizeEvent = functions.database
     .ref("/Events/{gameName}/{event}")
-    .onCreate((snap, context) => {
+    .onCreate(async (snap, context) => {
       const event = snap.val();
       const ref = admin.firestore().collection("users")
           .doc(context.auth?.uid ?? "");
+      const user = await admin.auth().getUser(context.auth?.uid ?? "");
+      snap.ref.child("Permissions").child(context.auth?.uid ?? "").set({
+        "role": "admin",
+        "name": user.displayName,
+        "email": user.email,
+        "photoURL": user.photoURL,
+      });
       return admin.firestore().runTransaction(async (t) => {
         const doc = await t.get(ref);
         const events = doc.data()?.events;
@@ -101,6 +113,7 @@ export const nativizeEvent = functions.database
       });
     });
 
+// Delete event from user's inbox
 export const deleteEvent = functions.database
     .ref("/Events/{gameName}/{event}")
     .onDelete((snap) => {
@@ -136,6 +149,7 @@ export const createUser = functions.auth.user().onCreate(async (user) => {
   });
 });
 
+// Delete user's document
 export const deleteUser = functions.auth.user().onDelete(async (user) => {
   return admin.firestore().collection("users").doc(user.uid).delete();
 });
