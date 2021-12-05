7db62fad438c2caa17dfa42afc07ffb068b42f70
ayuram <54257595+ayuram@users.noreply.github.com>
2021-12-05 11:27:18 -0800

    remove users

--- lib/views/home/util/Permissions.dart
+++ lib/views/home/util/Permissions.dart
@@ -2,6 +2,7 @@ import 'package:firebase_database/firebase_database.dart' as Database;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:teamtrack/components/PFP.dart';
 import 'package:teamtrack/components/PlatformGraphics.dart';
 import 'package:teamtrack/models/AppModel.dart';
@@ -25,45 +26,87 @@ class Permissions extends StatefulWidget {
 }
 
 class _PermissionsState extends State<Permissions> {
+  final slider = SlidableDrawerActionPane();
   @override
   Widget build(BuildContext context) => ListView(
         children: widget.users
-            .map(
-              (user) => ListTile(
-                leading: PFP(user: user),
-                title: PlatformText(
-                  user.displayName ?? "Unknown",
-                ),
-                subtitle: PlatformText(
-                  user.email ?? "Unknown",
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                trailing: DropdownButton<Role>(
-                  value: user.role,
-                  items: Role.values
-                      .map(
-                        (e) => DropdownMenuItem<Role>(
-                          child: PlatformText(
-                            e.name(),
-                          ),
-                          value: e,
-                        ),
-                      )
-                      .toList(),
-                  onChanged: (widget.event.role == Role.admin &&
-                          widget.currentUser?.uid != user.uid)
-                      ? (newValue) {
-                          HapticFeedback.lightImpact();
-                          setState(
-                            () => widget.ref
-                                ?.child('${user.uid}/role')
-                                .set(newValue?.toRep()),
-                          );
-                        }
-                      : null,
-                ),
-              ),
-            )
+            .map((user) => Slidable(
+                  actionPane: slider,
+                  secondaryActions: [
+                    IconSlideAction(
+                      onTap: widget.event.role == Role.admin
+                          ? () {
+                              showPlatformDialog(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    PlatformAlert(
+                                  title: PlatformText('Remove User'),
+                                  content: PlatformText('Are you sure?'),
+                                  actions: [
+                                    PlatformDialogAction(
+                                      isDefaultAction: true,
+                                      child: PlatformText('Cancel'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    PlatformDialogAction(
+                                      isDefaultAction: false,
+                                      isDestructive: true,
+                                      child: PlatformText('Confirm'),
+                                      onPressed: () {
+                                        setState(
+                                          () => widget.ref
+                                              ?.child('${user.uid}')
+                                              .remove(),
+                                        );
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            }
+                          : null,
+                      icon: Icons.delete,
+                      color: Colors.red,
+                    )
+                  ],
+                  child: ListTile(
+                    leading: PFP(user: user),
+                    title: PlatformText(
+                      user.displayName ?? "Unknown",
+                    ),
+                    subtitle: PlatformText(
+                      user.email ?? "Unknown",
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    trailing: DropdownButton<Role>(
+                      value: user.role,
+                      items: Role.values
+                          .map(
+                            (e) => DropdownMenuItem<Role>(
+                              child: PlatformText(
+                                e.name(),
+                              ),
+                              value: e,
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (widget.event.role == Role.admin &&
+                              widget.currentUser?.uid != user.uid)
+                          ? (newValue) {
+                              HapticFeedback.lightImpact();
+                              setState(
+                                () => widget.ref
+                                    ?.child('${user.uid}/role')
+                                    .set(newValue?.toRep()),
+                              );
+                            }
+                          : null,
+                    ),
+                  ),
+                ))
             .toList(),
       );
 }
