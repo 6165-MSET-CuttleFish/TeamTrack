7e72bed265d93adb2aa66adcac61fb66fa42e5fe
Aadit Noronha <aadit.noronha@gmail.com>
2023-09-04 17:12:15 -0700

    My first commit

---
+++ lib/views/home/team/AllianceSelection.dart
@@ -0,0 +1,283 @@
+import 'package:teamtrack/components/misc/EmptyList.dart';
+import 'package:teamtrack/models/GameModel.dart';
+import 'package:teamtrack/models/ScoreModel.dart';
+import 'package:teamtrack/models/StatConfig.dart';
+import 'package:teamtrack/views/home/team/ExampleTeamRow.dart';
+import 'package:teamtrack/views/home/team/TeamRow.dart';
+import 'package:teamtrack/views/home/team/TeamView.dart';
+import 'package:teamtrack/models/AppModel.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:teamtrack/components/misc/PlatformGraphics.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:teamtrack/functions/Statistics.dart';
+import 'dart:convert';
+
+class TeamList extends StatefulWidget {
+  TeamList({
+    super.key,
+    required this.event,
+    required this.sortMode,
+    required this.statConfig,
+    required this.elementSort,
+    required this.statistic,
+  });
+  final Event event;
+  final OpModeType? sortMode;
+  final ScoringElement? elementSort;
+  final StatConfig statConfig;
+  final Statistics statistic;
+  @override
+  State<TeamList> createState() => _TeamList();
+}
+
+class _TeamList extends State<TeamList> {
+  @override
+  Widget build(BuildContext context) => StreamBuilder<DatabaseEvent>(
+        stream: widget.event.getRef()?.onValue,
+        builder: (context, eventHandler) {
+          if (eventHandler.hasData && !eventHandler.hasError) {
+            widget.event.updateLocal(
+              json.decode(
+                json.encode(
+                  eventHandler.data?.snapshot.value,
+                ),
+              ),
+              context,
+            );
+          }
+          if (!eventHandler.hasData && widget.event.shared) {
+            return Center(
+              child: PlatformProgressIndicator(),
+            );
+          }
+          var max = widget.event.teams.maxCustomStatisticScore(
+            Dice.none,
+            widget.statConfig.removeOutliers,
+            widget.statistic,
+            widget.sortMode,
+            widget.elementSort,
+          );
+          final teams = widget.statConfig.sorted
+              ? widget.event.teams.sortedTeams(
+                  widget.sortMode,
+                  widget.elementSort,
+                  widget.statConfig,
+                  widget.event.matches.values.toList(),
+                  widget.statistic,
+                )
+              : widget.event.teams.orderedTeams();
+          if (widget.statConfig.allianceTotal) {
+            max = teams
+                .map(
+                  (e) => widget.event.matches.values
+                      .toList()
+                      .spots(e, Dice.none, widget.statConfig.showPenalties,
+                          type: widget.sortMode)
+                      .removeOutliers(widget.statConfig.removeOutliers)
+                      .map((spot) => spot.y)
+                      .getStatistic(widget.statistic.getFunction()),
+                )
+                .maxValue();
+          }
+          if (teams.length == 0) return EmptyList();
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: ExampleTeamRow(
+                  sortMode: widget.sortMode,
+                  elementSort: widget.elementSort,
+                  statistics: widget.statistic,
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: teams.length,
+                  itemBuilder: (context, index) {
+                    final team = teams[index];
+                    return Slidable(
+                      child: TeamRow(
+                        team: team,
+                        event: widget.event,
+                        sortMode: widget.sortMode,
+                        statConfig: widget.statConfig,
+                        elementSort: widget.elementSort,
+                        max: max,
+                        statistics: widget.statistic,
+                        onTap: () async {
+                          await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TeamView(
+                                team: team,
+                                event: widget.event,
+                              ),
+                            ),
+                          );
+                          setState(() {});
+                        },
+                      ),
+                      endActionPane: ActionPane(
+                        // A motion is a widget used to control how the pane animates.
+                        motion: const StretchMotion(),
+                        children: [
+                          SlidableAction(
+                            icon: Icons.delete,
+                            backgroundColor: Colors.red,
+                            onPressed: (_) {
+                              showPlatformDialog(
+                                context: context,
+                                builder: (BuildContext context) =>
+                                    PlatformAlert(
+                                  title: Text('Delete Team'),
+                                  content: Text('Are you sure?'),
+                                  actions: [
+                                    PlatformDialogAction(
+                                      isDefaultAction: true,
+                                      child: Text('Cancel'),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    PlatformDialogAction(
+                                      isDefaultAction: false,
+                                      isDestructive: true,
+                                      child: Text('Confirm'),
+                                      onPressed: () {
+                                        String? s;
+                                        setState(() {
+                                          s = widget.event.deleteTeam(team);
+                                        });
+                                        dataModel.saveEvents();
+                                        Navigator.of(context).pop();
+                                        if (s != null)
+                                          showPlatformDialog(
+                                            context: context,
+                                            builder: (context) => PlatformAlert(
+                                              title: Text('Error'),
+                                              content: Text(
+                                                  'Team is present in matches'),
+                                              actions: [
+                                                PlatformDialogAction(
+                                                  child: Text('Okay'),
+                                                  isDefaultAction: true,
+                                                  onPressed: () =>
+                                                      Navigator.of(context)
+                                                          .pop(),
+                                                )
+                                              ],
+                                            ),
+                                          );
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      );
+}
+
+class TeamSearch extends SearchDelegate<String?> {
+  TeamSearch({
+    required this.teams,
+    required this.event,
+    this.sortMode,
+    required this.statConfig,
+    required this.elementSort,
+    required this.statistics,
+  }) {
+    max = event.teams.maxCustomStatisticScore(Dice.none,
+        statConfig.removeOutliers, statistics, sortMode, elementSort);
+    final teams = event.teams.values;
+    if (statConfig.allianceTotal) {
+      max = teams
+          .map(
+            (e) => event.matches.values
+                .toList()
+                .spots(e, Dice.none, statConfig.showPenalties, type: sortMode)
+                .removeOutliers(statConfig.removeOutliers)
+                .map((spot) => spot.y)
+                .getStatistic(statistics.getFunction()),
+          )
+          .maxValue();
+    }
+  }
+  late double max;
+  OpModeType? sortMode;
+  ScoringElement? elementSort;
+  List<Team> teams;
+  Event event;
+  StatConfig statConfig;
+  Statistics statistics;
+
+  @override
+  List<Widget> buildActions(BuildContext context) => [
+        if (query.isNotEmpty)
+          IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => query = '',
+          ),
+      ];
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () => close(context, null),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) => buildSuggestions(context);
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isNotEmpty
+        ? [
+            ...teams.where(
+              (q) => q.number.contains(query),
+            ),
+            ...teams.where(
+              (q) => q.name.toLowerCase().contains(query.toLowerCase()),
+            ),
+          ].toList()
+        : teams;
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) => TeamRow(
+        statistics: statistics,
+        statConfig: statConfig,
+        team: suggestionList[index],
+        event: event,
+        max: max,
+        sortMode: sortMode,
+        elementSort: elementSort,
+        onTap: () async {
+          close(context, null);
+          await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TeamView(
+                team: suggestionList[index],
+                event: event,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/views/home/team/TeamRowAlliance.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:teamtrack/functions/Statistics.dart';
+import 'package:teamtrack/models/GameModel.dart';
+import 'package:teamtrack/models/ScoreModel.dart';
+import 'package:teamtrack/functions/Extensions.dart';
+
+class ExampleTeamRow extends StatelessWidget {
+  ExampleTeamRow(
+      {super.key, this.sortMode, this.elementSort, required this.statistics});
+  final Team team = Team('Team #', 'Team Name');
+  final OpModeType? sortMode;
+  final ScoringElement? elementSort;
+  final Statistics statistics;
+  Color wltColor(int i) {
+    if (i == 0) {
+      return Colors.green;
+    } else if (i == 1) {
+      return Colors.red;
+    } else {
+      return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(context) {
+    final elementName = elementSort?.name ?? "";
+    final wlt = ['W', 'L', 'T'];
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
+        ),
+      ),
+      child: ListTile(
+        tileColor: Colors.grey.withOpacity(0.3),
+        title: SizedBox(
+          width: 200,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                team.name,
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              Row(
+                children: [
+                  for (int i = 0; i < wlt.length; i++)
+                    Row(
+                      children: [
+                        Text(
+                          wlt[i],
+                          style: TextStyle(color: wltColor(i), fontSize: 12),
+                        ),
+                        if (i < wlt.length - 1) Text('-')
+                      ],
+                    ),
+                ],
+              )
+            ],
+          ),
+        ),
+        leading: Text(
+          team.number,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 80,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      '${elementName.isEmpty ? sortMode.getName(shortened: true) : elementName} '),
+                  Text('% Change'),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(
+                10,
+              ),
+            ),
+            Text('% Max ${statistics.name}')
+          ],
+        ),
+      ),
+    );
+  }
+}
